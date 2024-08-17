9fa7f6c6deb4ff278165420536d946ec90f38ea1
Jean Lucas <j3anlucas010@gmail.com>
2024-08-17 18:14:02 -0300

    Jogo de ganhar pontos

---
+++ script.js
@@ -0,0 +1,13 @@
+let numero=Number(Math.random().toFixed())
+const j1=50
+const j2=5
+
+if(j1==j2) console.log('empate')
+
+else{
+        if(numero==0) console.log(`O jogador 1 ganhou 1 ponto `)
+        if(numero==1) console.log(`O jogador 2 ganhou 1 ponto `)        
+}
+
+
+
